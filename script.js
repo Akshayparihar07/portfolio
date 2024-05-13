2fd030cab2aedd19a6ce68a8accfc2cb40e4c780
Akshay Parihar <pariharakshay40@gmail.com>
2024-05-14 02:41:38 +0530

    Modified Project Section

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const openHamButton = document.querySelector('.open-hamburger')
 const closeHamButton = document.querySelector('.close-hamburger')
 const hamburgerMenu = document.querySelector('.hamburger-menu')
-openHamButton.addEventListener('click', ()=> hamburgerMenu.style.right = '0px');
-closeHamButton.addEventListener('click', ()=>hamburgerMenu.style.right = '-1500px')
+openHamButton.addEventListener('click', () => hamburgerMenu.style.right = '0px');
+closeHamButton.addEventListener('click', () => hamburgerMenu.style.right = '-1500px')
